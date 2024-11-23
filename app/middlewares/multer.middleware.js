20b76c34c0098bdc431081d74c474567fecf7b46
DeveloperMokchhedul <mokchheduls46@gmail.com>
2024-11-23 07:55:38 +0600

    multer update

--- app/middlewares/multer.middleware.js
+++ app/middlewares/multer.middleware.js
@@ -1,55 +1,60 @@
-// import multer from "multer";
-// import fs from "fs";
+import multer from "multer";
+import fs from "fs";
 
-
-// const createFolderIfNotExists = (folderPath) => {
-//     if (!fs.existsSync(folderPath)) {
-//         fs.mkdirSync(folderPath, { recursive: true }); 
-//     }
-// };
-
-
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-    
-//         const folderPath = `./public/${req.body.folderName || "temp"}`;
-//         createFolderIfNotExists(folderPath); 
-//         cb(null, folderPath);
-//     },
-
-//     filename: function (req, file, cb) {
-//         const uniqueName = `${Date.now()}-${file.originalname}`; 
-//         cb(null, uniqueName);
-//     },
-// });
-
-// export const upload = multer({ storage: storage });
-
-
-
-
-
-
-
-
-import multer from "multer"
-
-
-// file
-const storage = multer.diskStorage({
-    //to upload file on destionation
-    destination:function(req,file,cb){
-            cb(null,"/upload")
-    },
-
-    
-
-
-//to configure file name 
-    filename: function(req,file,cb){
-        cb(null, file.originalname)
+const createFolderIfNotExists = (folderPath) => {
+    if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath, { recursive: true });
     }
-})
+};
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const folderPath = "./upload";
+        createFolderIfNotExists(folderPath); 
+        cb(null, folderPath);
+    },
+    filename: function (req, file, cb) {
+        cb(null, `${Date.now()}-${file.originalname}`); 
+    },
+});
+
+export const upload = multer({ storage: storage });
 
 
-export const upload = multer({storage:storage})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import multer from "multer"
+
+
+// // file
+// const storage = multer.diskStorage({
+//     //to upload file on destionation
+//     destination:function(req,file,cb){
+//             cb(null,"/upload")
+//     },
+
+    
+
+
+// //to configure file name 
+//     filename: function(req,file,cb){
+//         cb(null, file.originalname)
+//     }
+// })
+
+
+// export const upload = multer({storage:storage})
