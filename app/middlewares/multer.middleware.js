d2e6735a87cbceb5623b0086414905329de1eb52
DeveloperMokchhedul <mokchheduls46@gmail.com>
2024-11-23 07:31:52 +0600

    multer and cloudinary update

--- app/middlewares/multer.middleware.js
+++ app/middlewares/multer.middleware.js
@@ -1,21 +1,55 @@
-import multer from "multer"
+import multer from "multer";
+import fs from "fs";
 
 
-// file
+const createFolderIfNotExists = (folderPath) => {
+    if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath, { recursive: true }); 
+    }
+};
+
+
 const storage = multer.diskStorage({
-    //to upload file on destionation
-    destination:function(req,file,cb){
-            cb(null,"./upload")
+    destination: function (req, file, cb) {
+    
+        const folderPath = `./public/${req.body.folderName || "temp"}`;
+        createFolderIfNotExists(folderPath); 
+        cb(null, folderPath);
     },
+
+    filename: function (req, file, cb) {
+        const uniqueName = `${Date.now()}-${file.originalname}`; 
+        cb(null, uniqueName);
+    },
+});
+
+export const upload = multer({ storage: storage });
+
+
+
+
+
+
+
+
+// import multer from "multer"
+
+
+// // file
+// const storage = multer.diskStorage({
+//     //to upload file on destionation
+//     destination:function(req,file,cb){
+//             cb(null,"./public/temp")
+//     },
 
     
 
 
-//to configure file name 
-    filename: function(req,file,cb){
-        cb(null, file.originalname)
-    }
-})
+// //to configure file name 
+//     filename: function(req,file,cb){
+//         cb(null, file.originalname)
+//     }
+// })
 
 
-export const upload = multer({storage:storage})
+// export const upload = multer({storage:storage})
